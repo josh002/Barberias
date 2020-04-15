6b3da869e930086f9e3522bbff683bd4004f4ac3
Josue Carrasco <jos247car@gmail.com>
2020-04-15 20:42:00 -0300

    pantallas y routing agregado

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,4 +1,4 @@
-  
+
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
@@ -11,69 +11,66 @@ var app = new Framework7({
     id: 'com.myapp.test',
     // Enable swipe panel
     panel: {
-      swipe: 'left',
+        swipe: 'left',
     },
     // Add default routes
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
+        {
+            path: '/home/',
+            url: './pages/user/home.html'
+        },
+        {
+            path: '/admin/',
+            url: './pages/admin/admin.html'
+        },
+        {
+            path: '/register/',
+            url: './pages/register.html'
+        },
+
     ]
     // ... other parameters
-  });
+});
 
 var mainView = app.views.create('.view-main');
 
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
+$$(document).on('deviceready', function () {
     console.log("Device is ready!");
-
-    $$('.tocaBoton').on('click', fnTocaBoton);
 
 });
 
+//LOGIN
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
-    console.log(e);
+    console.log(e,);
+    console.log('LOGIN')
 })
 
-// Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+//REGISTRO
+$$(document).on('page:init', '.page[data-name="register"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log(e);
-    alert('Hello');
+    console.log('REGISTRO')
+})
+//>>>>>>>>>>>>>> USER  <<<<<<<<<<<<<<<
+//HOME
+$$(document).on('page:init', '.page[data-name="home"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    console.log('HOME')
+
+})
+//>>>>>>>>>>>>>> ADMIN  <<<<<<<<<<<<<<<
+$$(document).on('page:init', '.page[data-name="admin"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    console.log('ADMIN')
+
 })
 
-
 /** FUNCIONES PROPIAS **/
-function fnTocaBoton() {
-    var mensaje = "";
-
-    idDelBoton = this.id;
-    mensaje = "Mi ID es: " + idDelBoton + "<br/>";
-
-    // voy a "separar" el valor del id, usando los guiones bajos
-    // el split separa un valor (en este caso una variable),
-    // usando el caracter o caracteres indicandos como parámetro
-    // el resultado es un array!
-    var partes = idDelBoton.split("_");
-
-    // sabiendo la forma: btn_g1_1 puedo tener:
-    p0 = partes[0];
-    p1 = partes[1];
-    p2 = partes[2];
-
-    mensaje += "Soy del Grupo: " + p1 + "<br/>";
-    mensaje += "Y tengo el nro: " + p2 + "<br/>";
-
-
-    $$('#msgBtn').html(mensaje);
-
-
-
-}
 
 
 
