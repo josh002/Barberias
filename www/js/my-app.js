71efc37ad2bf60a33ae48a05a402776f8b71459e
Josue Carrasco <jos247car@gmail.com>
2020-04-20 19:03:59 -0300

    login y registro realizados

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,28 +1,28 @@
 var $$ = Dom7;
 
 var app = new Framework7({
-  root: '#app', // App root element
+    root: '#app', // App root element
 
-  id: 'io.framework7.myapp', // App bundle ID
-  name: 'My App', // App name
-  theme: 'auto', // Automatic theme detection
-  // App root data
-  // App root methods
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
+    id: 'io.framework7.myapp', // App bundle ID
+    name: 'My App', // App name
+    theme: 'auto', // Automatic theme detection
+    // App root data
+    // App root methods
+    methods: {
+        helloWorld: function () {
+            app.dialog.alert('Hello World!');
+        },
     },
-  },
-  // App routes
-  routes: routes,
+    // App routes
+    routes: routes,
 
 
-  // Cordova Statusbar settings
-  statusbar: {
-    iosOverlaysWebView: true,
-    androidOverlaysWebView: false,
-  },
-  
+    // Cordova Statusbar settings
+    statusbar: {
+        iosOverlaysWebView: true,
+        androidOverlaysWebView: false,
+    },
+
 
 });
 
@@ -30,20 +30,12 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main');
 
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
+$$(document).on('deviceready', function () {
     console.log("Device is ready!");
-    $$('#registro').on('click', registro);
+    var db = firebase.firestore();
+    $$('#login').on('click', login);
 });
-function registro(){
-    var email = 'josueeeeeeeeeeeee@gmail.com';
-    var password = 'ojosdeangel';
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
-}
+
 
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
@@ -57,3 +49,47 @@ $$(document).on('page:init', '.page[data-name="about"]', function (e) {
     console.log(e);
     alert('Hello');
 })
+$$(document).on('page:init', '.page[data-name="form"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    $$('#register').on('click', register);
+})
+//FUNCTIONS
+//register
+function register() {
+    var hasError = false;
+    var email = $$('#email').val();
+    var password = $$('#password').val();
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(error);
+        hasError = true;
+    }).then(function (resp) {
+        if (!hasError) {
+            mainView.router.navigate("/");
+        }
+    });
+}
+//login
+function login() {
+    var hasError = false;
+    var email = $$('#email-sign').val();
+    var password = $$('#password-sign').val();
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(function (error) {
+            //Si hubo algun error, ponemos un valor referenciable en la variable huboError
+            hasError= true;
+            var errorCode = error.code;
+            var errorMessage = error.message;
+           alert(error);
+        })
+        .then(function () {
+            //En caso de que esté correcto el inicio de sesión y no haya errores, se dirige a la siguiente página
+            if (!hasError) {
+                mainView.router.navigate('/about/');
+            }
+        });
+
+}
