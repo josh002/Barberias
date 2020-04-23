afdd48e3ce36b176be783256e4d966244dce1999
Josue Carrasco <jos247car@gmail.com>
2020-04-23 15:22:42 -0300

    autologin agregado

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,4 +1,7 @@
 var $$ = Dom7;
+var storage = window.localStorage;
+var usuario = { "email": "", "clave": "" };
+var usuarioLocal, claveLocal;
 
 var app = new Framework7({
     root: '#app', // App root element
@@ -33,7 +36,8 @@ var mainView = app.views.create('.view-main');
 $$(document).on('deviceready', function () {
     console.log("Device is ready!");
     var db = firebase.firestore();
-   
+    consultarLocalStorage();
+
 });
 
 
@@ -63,7 +67,7 @@ $$(document).on('page:init', '.page[data-name="form"]', function (e) {
 //FUNCTIONS
 //register
 function register() {
-   
+
     var hasError = false;
     var email = $$('#email').val();
     var password = $$('#password').val();
@@ -90,16 +94,75 @@ function login() {
     firebase.auth().signInWithEmailAndPassword(logEmail, logPassword)
         .catch(function (error) {
             //Si hubo algun error, ponemos un valor referenciable en la variable huboError
-            logHasError= true;
+            logHasError = true;
             var errorCode = error.code;
             var errorMessage = error.message;
-           alert(error);
+            alert(error);
         })
         .then(function () {
             //En caso de que esté correcto el inicio de sesión y no haya errores, se dirige a la siguiente página
             if (!logHasError) {
+                usuario = { email: logEmail, clave: logPassword };
+                //storage.setItem("persona", persona); -> guardará [object Object]
+                var usuarioAGuardar = JSON.stringify(usuario);
+                // por eso convertimos el JSON en un string
+
+                storage.setItem("usuario", usuarioAGuardar);
+                console.log("usuarioAGuardar: " + usuarioAGuardar);
+                console.log("usuario: " + usuario.email + "password: " + usuario.clave);
+
                 mainView.router.navigate('/tabs/');
             }
         });
 
 }
+function consultarLocalStorage() {
+
+    var usuarioGuardado = storage.getItem("usuario");
+    usuarioGuardado = JSON.parse(usuarioGuardado);
+    // convertimos el string en JSON
+
+    if (usuarioGuardado.email == "") {
+        console.log("no hay datos en el local");
+    } else {
+
+        console.log(" usuarioguardado.email: " + usuarioGuardado.email);
+        console.log(" usuarioguardado.clave: " + usuarioGuardado.clave);
+        //pasar los datos del json a dos variables independientes
+        usuarioLocal = usuarioGuardado.email;
+        claveLocal = usuarioGuardado.clave;
+        console.log("usuariolocal + clavelocal: " + usuarioLocal + claveLocal)
+        //si la variable tiene datos llamamos a una funcion de login pasandole las variables como parametros
+
+        if (usuarioGuardado != null) {
+            LoguearseConLocal(usuarioLocal, claveLocal);
+        };
+    };
+};
+
+function LoguearseConLocal(u, c) {
+    console.log("loguearseconlocal, u+c" + u + c)
+
+    //Se declara la variable huboError (bandera)
+    var huboError = 0;
+    firebase.auth().signInWithEmailAndPassword(u, c)
+        .catch(function (error) {
+            //Si hubo algun error, ponemos un valor referenciable en la variable huboError
+            huboError = 1;
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.error(errorMessage);
+            console.log(errorCode);
+
+
+        })
+        .then(function () {
+            //En caso de que esté correcto el inicio de sesión y no haya errores, se dirige a la siguiente página
+            if (huboError == 0) {
+                console.log("te logueaste");
+                mainView.router.navigate('/tabs/');
+            }
+        });
+
+};
+
